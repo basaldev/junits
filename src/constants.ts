b9a954bb71a284c5a5549957b10614a7b896948f
Shinsuke Marty Yamada <marty@exodevhub.com>
2019-05-07 14:51:15 +0900

    Fix yobi bug #4

--- src/constants.ts
+++ src/constants.ts
@@ -1,31 +1,30 @@
-
-type Gengo = {
+interface IGengo {
   start: number[];
   full: string;
   short: string;
   alpha: string;
-};
+}
 
 type Yobi = string;
 
 type Shogo = string;
 
-type Date = {
-  gengo: Gengo[];
+interface IDate {
+  gengo: IGengo[];
   yobi: Yobi[];
   shogo: Shogo[];
-};
+}
 
-export const date: Date = {
+export const date: IDate = {
   gengo: [
     { start: [2019, 4, 1], full: '令和', short: '令', alpha: 'R' },
     { start: [1989, 0, 8], full: '平成', short: '平', alpha: 'H' },
     { start: [1926, 11, 25], full: '昭和', short: '昭', alpha: 'S' },
-    { start: [1912, 6 ,30], full: '大正', short: '大', alpha: 'T' },
+    { start: [1912, 6, 30], full: '大正', short: '大', alpha: 'T' },
     { start: [1868, 0, 1], full: '明治', short: '明', alpha: 'M' }
   ],
 
-  yobi: ['月', '火', '水', '木', '金', '土', '日'],
+  yobi: ['日', '月', '火', '水', '木', '金', '土'],
 
   shogo: ['午前', '午後']
 };
